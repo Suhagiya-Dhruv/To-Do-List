465333b77e34a15ec96390243ef53f75bf4d2982
dhruv <dhruv.suhagiya@gmail.com>
2021-04-27 22:47:13 +0530

    Using useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-
+import Person from './Person';
 function App() {
     return (
         <>
             <div className="App">
-                <h1> Hello React  </h1>
+                <Person name="Dhruv" age="21"></Person>
+                <Person name="Milan" age="22"></Person>
             </div>
         </>
     )
